14add45b228abde20551860cd96700ad52973e5d
Derek Batenhorst <djbaten@gmail.com>
2019-04-10 21:34:17 -0500

    Started click counter example and added unit tests.

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,28 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import Enzyme, { shallow } from 'enzyme';
+import EnzymeAdapter from 'enzyme-adapter-react-16';
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+Enzyme.configure({ adapter: new EnzymeAdapter() });
+
+test('renders without error', () => {
+  const wrapper = shallow(<App />);
+  const appComponent = wrapper.find('[data-test="component-app"]');
+  expect(appComponent.length).toBe(1);
+});
+test('renders increment button', () => {
+  const wrapper = shallow(<App />);
+  const button = wrapper.find('[data-test="increment-button"]');
+  expect(button.length).toBe(1);
+});
+test('renders counter display', () => {
+  const wrapper = shallow(<App />);
+  const counterDisplay = wrapper.find('[data-test="counter-display"]');
+  expect(counterDisplay.length).toBe(1);
+});
+test('counter starts at 0', () => {
+
+});
+test('clicking button increments counter display', () => {
+
 });
--- src/App.tsx
+++ src/App.tsx
@@ -5,21 +5,9 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div data-test="component-app">
+        <h1 data-test="counter-display">The counter is currently</h1>
+        <button data-test="increment-button">Increment counter</button>
       </div>
     );
   }
