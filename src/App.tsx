275e259c39ec986e6e403401e56773da9ad89d38
Derek Batenhorst <djbaten@gmail.com>
2019-04-17 21:17:05 -0500

    Finished section 2 of course.

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,35 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
+class App extends Component<any, { counter: number, errorMsg?: string | null }> {
+  constructor(props: any) {
+    super(props);
+
+    this.state = {
+      counter: 0
+    };
+  }
+
+
+
+  decrementCounter = () => {
+    if (this.state.counter > 0) {
+      this.setState(p => ({ counter: p.counter - 1 }));
+    } else {
+      this.setState({ ...this.state, errorMsg: 'Counter cannot be less than 0.' });
+    }
+  }
+
   render() {
     return (
       <div data-test="component-app">
-        <h1 data-test="counter-display">The counter is currently</h1>
-        <button data-test="increment-button">Increment counter</button>
+        <h1 data-test="counter-display">The counter is currently {this.state.counter}</h1>
+        {this.state.errorMsg && <h2 data-test="error-message" style={{ color: 'red' }}>{this.state.errorMsg}</h2>}
+        <button
+          data-test="increment-button"
+          onClick={() => this.setState(p => ({ counter: p.counter + 1, errorMsg: null }))}>Increment counter</button>
+        <button data-test="decrement-button"
+          onClick={this.decrementCounter}>Decrement counter</button>
       </div>
     );
   }
