eebd56e6283d7455be84ac38933fc06f1ed66637
Derek Batenhorst <djbaten@gmail.com>
2019-04-24 21:45:34 -0500

    Adding Congrats component along with tests.

---
+++ src/Congrats.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+export interface ICongratsProps {
+    success?: boolean;
+}
+
+export default (props: ICongratsProps) => {
+    if (props.success) {
+        return (
+            <div data-test="component-test">
+                <span data-test="congrats-message">
+                    Congratulations! You guessed the word!
+                </span>
+            </div>
+        );
+    }
+    return (
+        <div data-test="component-congrats" />
+    );
+}
---
+++ test/testUtils.ts
@@ -0,0 +1,6 @@
+import { ShallowWrapper } from "enzyme";
+
+
+export const findByTestAttr = (wrapper: ShallowWrapper, val: string) => {
+    return wrapper.find(`[data-test="${val}"]`);
+}
